36d8d8adcb2143fc763891f29e3a53c1d0c016d7
Rémi Parpaillon <remi.parpaillon@gmail.com>
2015-12-19 11:17:12 +0100

    Move the old main to guess.rs
    The new main will be used for the dining philosophers.

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,19 @@
-extern crate rand;
+struct Philosopher {
+    name: String,
+}
 
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-
-fn main() {
-    println!("Welcome to the guessing game");
-    println!("Please input your guess");
-
-    let secret_number = rand::thread_rng()
-        .gen_range(1, 101);
-
-    println!("Secret number is {}", secret_number);
-
-    loop {
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        println!("You guessed: {}", guess);
-
-        let guess: u32 = guess
-            .trim()
-            .parse()
-            .expect("Please type a number!");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less    => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal   => {
-                println!("You win!");
-                break;
-            },
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
         }
     }
+}
+
+fn main() {
+    let p1 = Philosopher::new("Judith Butler");
+    let p2 = Philosopher::new("Gilles Deleuze");
+    let p3 = Philosopher::new("Karl Marx");
+    let p4 = Philosopher::new("Emma Goldman");
+    let p5 = Philosopher::new("Michel Foucault");
 }
